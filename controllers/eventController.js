a204f7703776e9fcaed30576d0fb96218c04224d
Carlos <kdela.rocks@gmail.com>
2022-09-02 18:24:07 -0500

    lgm

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -8,7 +8,10 @@ const eventSchema = Joi.object({
     description: Joi.string(),
     slug: Joi.string().min(1).max(100).pattern(/^[a-z0-9_-]*$/).lowercase().required(),
     address: Joi.string().when('location', {is: 'in_person', then: Joi.required()}),
-    host: Joi.number().integer().positive()
+    durationInMinutes: Joi.number().integer().positive(),
+    host: Joi.number().integer().positive(),
+    startDateRange: Joi.date().required(),
+    endDateRange: Joi.date().required()
 });
 
 module.exports.new = (req,res) => {
@@ -19,37 +22,52 @@ module.exports.new = (req,res) => {
 
 module.exports.create = async (req,res) => {
     console.log(req.body);
-    let {username, id} = req.session.user;
-    let {name, location, description, slug, address} = req.body;
+    let {id: host} = req.session.user;
+    let {name,
+        location, 
+        description, 
+        slug, 
+        address, 
+        duration, 
+        timeUnit,
+        startDateRange,
+        endDateRange
+    } = req.body;
 
-    let host = Number(id);
+    console.log(startDateRange, new Date());
 
-    const {error, value} = eventSchema.validate({
+    //console.log(typeof id);
+    //let host = Number(id);
+    let durationInMinutes = duration;
+
+    if(timeUnit === 'hrs'){
+        durationInMinutes = durationInMinutes * 60;
+    }
+
+    const {error, value: data} = eventSchema.validate({
         name,
         location,
         description,
         slug,
         host,
-        address
+        address,
+        durationInMinutes,
+        startDateRange,
+        endDateRange
     });
 
-    console.log(error, value);
+    // console.log(error, value);
     if(error){
         console.log(error);
         return;
     }
 
+    //console.log('the value', value);
+
 
     try{
         const newEvent = await db.event.create({
-            data: {
-                name,
-                location,
-                description,
-                slug,
-                host: Number(id),
-                address
-            }
+            data
         });
 
         console.log(newEvent);
